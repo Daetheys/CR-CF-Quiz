ef96a4ca73b9cc4b2ac1553ca858b8b366b9ca0f
bsgarcia <basilegarcia@gmail.com>
2022-09-09 16:44:52 +0200

    update

--- dataset.js
+++ dataset.js
@@ -2,51 +2,51 @@ export const dataset = {
 	name: "Cushman",
 	instructions: [
 
-		// {
-			// title: "Consent",
-			// items: [
-//				{
-//					text: `You are about to participate in a psychological research study.
-//					This study aims to understand how people make moral decisions in a variety of situations.
-//					The proposed experiments have no immediate application or clinical value, but they will allow us to improve our understanding of moral reasoning in humans. We are asking you to participate in this study because you have been recruited on the Prolific platform.`,
-//					type: "regular",
-//					variables: [],
-//					title: "Information for the participant"
-//				},
-//				{
-//					text: `During your participation in this study, we will ask you to answer several simple questionnaires and tests, which do not require any particular competence.Your internet-based participation will require approximately 1 hour or  possibly less.`,
-//					type: "regular",
-//					variables: [],
-//					title: "Procedure"
-//				},
-//				{
-//					text: `Your participation in this study is voluntary. This means that you are consenting to participate in this project without external pressure.During your participation in this project, the researcher in charge and his staff will collect and record information about you. In order to preserve your identity and the confidentiality of the data, the identification of each file will be coded, thus preserving the anonymity of your answers. We will not collect any personal data from the RISC or Prolific platforms. The researcher in charge of this study will only use the data for research purposes in order to answer the scientific objectives of the project.The data may be published in scientific journals and shared within the scientific community, in which case no publication or scientific communication will contain identifying information.`,
-//					type: "regular",
-//					variables: [],
-//					title: "Voluntary Participation And Confidentiality"
-//				},
-//				{
-//					text: `This study contains fictional content (violence, sexual assault) that may be triggering for some people. By continuing, you are acknowledging that you agree to proceed. If you choose to continue, but end up being disturbed during the study, don't hesitate to exit the study at any time.`,
-//					type: 'alert',
-//					variables: [],
-//					title: "Trigger Warning"
-//				},
-//				{
-//					text: "I'm 18 years old or older",
-//					type: "checkbox",
-//				},
-//				{
-//					text: "My participation in this experiment is voluntary",
-//					type: "checkbox",
-//				},
-//				{
-//					text: "I understand that my data will be kept confidential and I can stop at any time without justification",
-//					type: "checkbox",
-//				}
-//			],
-//			type: "short",
-//		},
-//
+		{
+			title: "Consent",
+			items: [
+				{
+					text: `You are about to participate in a psychological research study.
+					This study aims to understand how people make moral decisions in a variety of situations.
+					The proposed experiments have no immediate application or clinical value, but they will allow us to improve our understanding of moral reasoning in humans. We are asking you to participate in this study because you have been recruited on the Prolific platform.`,
+					type: "regular",
+					variables: [],
+					title: "Information for the participant"
+				},
+				{
+					text: `During your participation in this study, we will ask you to answer several simple questionnaires and tests, which do not require any particular competence.Your internet-based participation will require approximately 1 hour or  possibly less.`,
+					type: "regular",
+					variables: [],
+					title: "Procedure"
+				},
+				{
+					text: `Your participation in this study is voluntary. This means that you are consenting to participate in this project without external pressure.During your participation in this project, the researcher in charge and his staff will collect and record information about you. In order to preserve your identity and the confidentiality of the data, the identification of each file will be coded, thus preserving the anonymity of your answers. We will not collect any personal data from the RISC or Prolific platforms. The researcher in charge of this study will only use the data for research purposes in order to answer the scientific objectives of the project.The data may be published in scientific journals and shared within the scientific community, in which case no publication or scientific communication will contain identifying information.`,
+					type: "regular",
+					variables: [],
+					title: "Voluntary Participation And Confidentiality"
+				},
+				{
+					text: `This study contains fictional content (violence, sexual assault) that may be triggering for some people. By continuing, you are acknowledging that you agree to proceed. If you choose to continue, but end up being disturbed during the study, don't hesitate to exit the study at any time.`,
+					type: 'alert',
+					variables: [],
+					title: "Trigger Warning"
+				},
+				{
+					text: "I'm 18 years old or older",
+					type: "checkbox",
+				},
+				{
+					text: "My participation in this experiment is voluntary",
+					type: "checkbox",
+				},
+				{
+					text: "I understand that my data will be kept confidential and I can stop at any time without justification",
+					type: "checkbox",
+				}
+			],
+			type: "short",
+		},
+
 		{
 			title: "Instructions",
 			items: [
